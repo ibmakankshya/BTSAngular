22d28a93646211071b4cfb8c0e1151bf78fdbb24
unknown <ibmjavanh06@iiht.tech>
2021-04-19 14:26:23 +0530

    bts and mysql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
